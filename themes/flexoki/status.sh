a279702eac799f7ed2b5c63faa58b0d27655faa6
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-10-31 11:08:44 -0300

    style(flexoki): align window colors with green palette

--- themes/flexoki/status.sh
+++ themes/flexoki/status.sh
@@ -22,7 +22,7 @@ tmux set -g @prefix_highlight_copy_mode_attr "fg=${PALETTE[cyan]},bg=${PALETTE[b
 #+ Status +  
 #+--------+
 #+--- Bars ---+
-tmux set -g status-left "#[fg=${PALETTE[bg]},bg=${PALETTE[cyan]},bold] 󰌌 #S #[fg=${PALETTE[cyan]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]"
+tmux set -g status-left "#[fg=${PALETTE[bg]},bg=${PALETTE[green]},bold] 󰌌 #S #[fg=${PALETTE[green]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]"
 tmux set -g status-right "#{prefix_highlight}#[fg=${PALETTE[bg_highlight]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]#[fg=${PALETTE[fg]},bg=${PALETTE[bg_highlight]}] 󰃭 ${DATE_FORMAT} #[fg=${PALETTE[fg]},bg=${PALETTE[bg_highlight]},nobold,noitalics,nounderscore]#[fg=${PALETTE[fg]},bg=${PALETTE[bg_highlight]}] 󰥔 ${TIME_FORMAT} #[fg=${PALETTE[cyan]},bg=${PALETTE[bg_highlight]},nobold,noitalics,nounderscore]#[fg=${PALETTE[bg]},bg=${PALETTE[cyan]},bold] #H "
 
 #+--- Windows ---+
