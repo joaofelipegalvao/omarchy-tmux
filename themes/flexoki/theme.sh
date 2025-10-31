1a6a0b1978ffe2fd86cb19459642a8515a28d38a
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-10-31 10:29:12 -0300

    refactor: fix flexoki theme file organization

--- themes/flexoki/theme.sh
+++ themes/flexoki/theme.sh
@@ -4,28 +4,43 @@ if [ ${#PALETTE[@]} -eq 0 ]; then
   echo "Warning: Flexoki Light palette not loaded. Colors may not display correctly."
 fi
 
-DATE_FORMAT=$(tmux show-environment -g THEME_DATE_FORMAT 2>/dev/null | cut -d= -f2-)
-TIME_FORMAT=$(tmux show-environment -g THEME_TIME_FORMAT 2>/dev/null | cut -d= -f2-)
-
-DATE_FORMAT=${DATE_FORMAT:-"%Y-%m-%d"}
-TIME_FORMAT=${TIME_FORMAT:-"%H:%M"}
-
 #+----------------+
 #+ Plugin Support +
 #+----------------+
 #+--- tmux-prefix-highlight ---+
-tmux set -g @prefix_highlight_output_prefix "#[fg=${PALETTE[cyan]}]#[bg=${PALETTE[bg1]}]#[nobold]#[noitalics]#[nounderscore]#[bg=${PALETTE[cyan]}]#[fg=${PALETTE[bg]}]"
-tmux set -g @prefix_highlight_output_suffix ""
-tmux set -g @prefix_highlight_copy_mode_attr "fg=${PALETTE[cyan]},bg=${PALETTE[bg1]},bold"
+tmux set -g @prefix_highlight_fg "${PALETTE[bg]}"
+tmux set -g @prefix_highlight_bg "${PALETTE[green]}"
+
+#+---------+
+#+ Options +
+#+---------+
+tmux set -g status-interval 1
+tmux set -g status on
 
 #+--------+
 #+ Status +
 #+--------+
-#+--- Bars ---+
-tmux set -g status-left "#[fg=${PALETTE[bg]},bg=${PALETTE[cyan]},bold] 󰌌 #S #[fg=${PALETTE[cyan]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]"
-tmux set -g status-right "#{prefix_highlight}#[fg=${PALETTE[bg_highlight]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]#[fg=${PALETTE[fg]},bg=${PALETTE[bg_highlight]}] 󰃭 ${DATE_FORMAT} #[fg=${PALETTE[fg]},bg=${PALETTE[bg_highlight]},nobold,noitalics,nounderscore]#[fg=${PALETTE[fg]},bg=${PALETTE[bg_highlight]}] 󰥔 ${TIME_FORMAT} #[fg=${PALETTE[cyan]},bg=${PALETTE[bg_highlight]},nobold,noitalics,nounderscore]#[fg=${PALETTE[bg]},bg=${PALETTE[cyan]},bold] #H "
+#+--- Layout ---+
+tmux set -g status-justify left
 
-#+--- Windows ---+
-tmux set -g window-status-format "#[fg=${PALETTE[bg]},bg=${PALETTE[green]},nobold,noitalics,nounderscore] #[fg=${PALETTE[bg]},bg=${PALETTE[green]}]#I #[fg=${PALETTE[bg]},bg=${PALETTE[green]},nobold,noitalics,nounderscore] #[fg=${PALETTE[bg]},bg=${PALETTE[green]}]#W #F #[fg=${PALETTE[green]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]"
-tmux set -g window-status-current-format "#[fg=${PALETTE[bg1]},bg=${PALETTE[bg5]},nobold,noitalics,nounderscore] #[fg=${PALETTE[green_2]},bg=${PALETTE[bg5]}]#I #[fg=${PALETTE[green_2]},bg=${PALETTE[bg5]},nobold,noitalics,nounderscore] #[fg=${PALETTE[green_2]},bg=${PALETTE[bg5]}]#W #F #[fg=${PALETTE[bg5]},bg=${PALETTE[bg1]},nobold,noitalics,nounderscore]"
-tmux set -g window-status-separator ""
+#+--- Colors ---+
+tmux set -g status-style "bg=${PALETTE[bg1]},fg=${PALETTE[fg]}"
+
+#+-------+
+#+ Panes +
+#+-------+
+tmux set -g pane-border-style "bg=default,fg=${PALETTE[base_150]}"
+tmux set -g pane-active-border-style "bg=default,fg=${PALETTE[blue]}"
+tmux set -g display-panes-colour "${PALETTE[bg]}"
+tmux set -g display-panes-active-colour "${PALETTE[fg_gutter]}"
+
+#+------------+
+#+ Clock Mode +
+#+------------+
+tmux setw -g clock-mode-colour "${PALETTE[blue]}"
+
+#+----------+
+#+ Messages +
+#+---------+
+tmux set -g message-style "bg=${PALETTE[bg1]},fg=${PALETTE[fg]}"
+tmux set -g message-command-style "bg=${PALETTE[base_150]},fg=${PALETTE[fg]}"
